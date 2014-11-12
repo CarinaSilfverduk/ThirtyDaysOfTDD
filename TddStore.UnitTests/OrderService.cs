49c72c54d709ac5a2a3ab82882a07906b36a3780
CarinaSilfverduk <carina.silfverduk@gmail.com>
2014-11-12 16:26:56 -0500

    Day 11 - Sometimes to Move Forward, you need a Lincoln.

---
+++ TddStore.UnitTests/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddStore.UnitTests;
+
+namespace TddStore.Core
+{
+    public class OrderService
+    {
+        private IOrderDataService _orderDataService;
+
+        public OrderService(IOrderDataService orderDataService)
+        {
+            _orderDataService = orderDataService;
+        }
+
+        public object PlaceOrder(Guid customerId, ShoppingCart shoppingCart)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TddStore.UnitTests/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TddStore.UnitTests
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void WhenUserPlacesACorrectOrderThenAnOrderNumberShouldBeReturned()
+        {
+            //Arrange
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.Items.Add(new ShoppingCartItem { ItemId = Guid.NewGuid(), Quantity = 1 });
+            var customerId = Guid.NewGuid();
+            var expectedOrderId = Guid.NewGuid();
+            var orderService = new OrderService();
+
+            //Act
+            var result = orderService.PlaceOrder(customerId, shoppingCart);
+
+            //Assert
+            Assert.AreEqual(expectedOrderId, result);
+        }
+    }
+}
